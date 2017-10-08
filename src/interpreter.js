bf28d5aee9d1375b7688242f681bdd1bb5ae8bc9
Alex <bessoalex@gmail.com>
2017-10-08 13:41:30 -0300

    Agregado de Facts y Rules

--- src/interpreter.js
+++ src/interpreter.js
@@ -70,6 +70,88 @@ var DBList = function(){
 };
 
 
+/* Definición de los Facts */
+var Fact = function(name){
+    this.name = name;
+    this.values = [];
+};
+Fact.prototype = new DBElement();
+Fact.prototype.verify= function(params){
+    var listOfValues = this.getValues();
+    var result = [];
+
+    function getResult(total, actual){
+        return (total == actual == true);
+    }
+
+    for (var key in params) {
+        result.push(params[key] === listOfValues[key]);
+    }
+
+    return result.reduce(getResult);
+};
+
+
+/* Definición de los Rules */
+var Rule = function(name,params){
+    this.name = name;
+    this.params = params;
+    this.values = [];
+};
+
+Rule.prototype = new DBElement();
+Rule.prototype.verify = function (posParams){
+    var _result = [];
+
+    function getValueOfParams(params){
+        var valuePos = [];
+        for (var key in params){
+            valuePos[key] = posParams[params[key]];
+        }
+        return valuePos;
+    }
+
+    function getResult(total, actual){
+        return (total == actual == true);
+    }
+
+    function verifyFacts(listOfFacts,params){
+        for (var key in _listOfFacts){
+            if (listOfFacts[key].verify(params)) return true;
+        }
+        return false;
+    }
+
+    var _db = new Database();
+
+    for (var key in this.values){
+        var _params = getValueOfParams(this.values[key].values);
+        var _listOfFacts = _db.facts.get(this.values[key].name);
+
+        _result.push(verifyFacts(_listOfFacts,_params));
+    }
+    return _result.reduce(getResult);
+};
+
+Rule.prototype.addValues = function(fname, fparams){
+    var dbElement = new DBElement();
+    dbElement.name = fname;
+
+    for (var key in fparams){
+        dbElement.values.push(this.params.indexOf(fparams[key]));
+    }
+    this.values.push(dbElement);
+};
+Rule.prototype.print = function(){
+        console.log("Name: " + this.name);
+        this.params.forEach(function(element) {
+            console.log("\t Params: " + element);
+        });
+        this.values.forEach(function(element) {
+            element.print();
+        });
+    };
+
 
 
 var Interpreter = function () {
